3f81edeb3cfef5de054182d8d8ac6a946b23f31e
dbyzero <cyril@dbyzero.com>
2015-05-24 17:09:37 -0700

    Improve import script

--- db_json/import.sh
+++ db_json/import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mongoimport -d deimos_test -c accounts < json/accounts.json
-mongoimport -d deimos_test -c avatars < json/avatars.json
-mongoimport -d deimos_test -c gameareas < json/gameareas.json
-mongoimport -d deimos_test -c itemtemplates < json/itemtemplates.json
-mongoimport -d deimos_test -c monstertemplates < json/monstertemplates.json
+mongoimport -d deimos_test --drop -c accounts < json/accounts.json
+mongoimport -d deimos_test --drop -c avatars < json/avatars.json
+mongoimport -d deimos_test --drop -c gameareas < json/gameareas.json
+mongoimport -d deimos_test --drop -c itemtemplates < json/itemtemplates.json
+mongoimport -d deimos_test --drop -c monstertemplates < json/monstertemplates.json
