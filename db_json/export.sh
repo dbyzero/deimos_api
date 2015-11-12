76be5475fd41a9f327f65c265b9c7a171cbe62ee
dbyzero <cyril@dbyzero.com>
2015-11-12 22:55:57 +0100

    Update db model expot/import

--- db_json/export.sh
+++ db_json/export.sh
@@ -4,3 +4,7 @@ mongoexport -d deimos_test -c avatars > json/avatars.json
 mongoexport -d deimos_test -c gameareas > json/gameareas.json
 mongoexport -d deimos_test -c itemtemplates > json/itemtemplates.json
 mongoexport -d deimos_test -c monstertemplates > json/monstertemplates.json
+mongoexport -d deimos_test -c effects > json/effects.json
+mongoexport -d deimos_test -c rectanglezonetemplates > json/rectanglezonetemplates.json
+mongoexport -d deimos_test -c spherezonetemplates > json/spherezonetemplates.json
+mongoexport -d deimos_test -c skills > json/skills.json
--- db_json/import.sh
+++ db_json/import.sh
@@ -4,3 +4,7 @@ mongoimport -d deimos_test --drop -c avatars < json/avatars.json
 mongoimport -d deimos_test --drop -c gameareas < json/gameareas.json
 mongoimport -d deimos_test --drop -c itemtemplates < json/itemtemplates.json
 mongoimport -d deimos_test --drop -c monstertemplates < json/monstertemplates.json
+mongoimport -d deimos_test --drop -c effects < json/effects.json
+mongoimport -d deimos_test --drop -c rectanglezonetemplates < json/rectanglezonetemplates.json
+mongoimport -d deimos_test --drop -c spherezonetemplates < json/spherezonetemplates.json
+mongoimport -d deimos_test --drop -c skills < json/skills.json
