67295982d99ef274ed616be4e02f4489c23f545a
dbyzero <cyril@dbyzero.com>
2016-04-07 18:14:11 +0200

    fix: change json path filename in import db script to match docker env

--- db_json/import.sh
+++ db_json/import.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mongoimport -d deimos_test --drop -c accounts < json/accounts.json
-mongoimport -d deimos_test --drop -c avatars < json/avatars.json
-mongoimport -d deimos_test --drop -c gameareas < json/gameareas.json
-mongoimport -d deimos_test --drop -c itemtemplates < json/itemtemplates.json
-mongoimport -d deimos_test --drop -c monstertemplates < json/monstertemplates.json
-mongoimport -d deimos_test --drop -c effects < json/effects.json
-mongoimport -d deimos_test --drop -c rectanglezonetemplates < json/rectanglezonetemplates.json
-mongoimport -d deimos_test --drop -c spherezonetemplates < json/spherezonetemplates.json
-mongoimport -d deimos_test --drop -c skills < json/skills.json
+mongoimport -d deimos_test --drop -c accounts < db_json/json/accounts.json
+mongoimport -d deimos_test --drop -c avatars < db_json/json/avatars.json
+mongoimport -d deimos_test --drop -c gameareas < db_json/json/gameareas.json
+mongoimport -d deimos_test --drop -c itemtemplates < db_json/json/itemtemplates.json
+mongoimport -d deimos_test --drop -c monstertemplates < db_json/json/monstertemplates.json
+mongoimport -d deimos_test --drop -c effects < db_json/json/effects.json
+mongoimport -d deimos_test --drop -c rectanglezonetemplates < db_json/json/rectanglezonetemplates.json
+mongoimport -d deimos_test --drop -c spherezonetemplates < db_json/json/spherezonetemplates.json
+mongoimport -d deimos_test --drop -c skills < db_json/json/skills.json
